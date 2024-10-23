7b2118ebacce76a67bee777f7fb2153d2a146a8a
Kamil-kml <125106948+Kamil-kml@users.noreply.github.com>
2024-10-23 21:30:11 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const navLogo = document.getElementById("nav-logo");
+const pictures = {
+    WasabiWondersLogo: 'Pictures/WasabiWondersLogo.png'
+}
+
+navLogo.innerHTML += `<img class="logo" src="${pictures.WasabiWondersLogo}" alt="Wasabi Wonders Logo" />`
